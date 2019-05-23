c3d92101583100f931c359838141e3cd45969ebb
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-05-23 09:08:30 +0200

    prepare it for ngrok

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var app = require('express')()
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
-server.listen(3000, '172.20.10.8')
+server.listen(3000, () => {
+  console.log('✅  The Server is running on port', 3000)
+})
 
 io.on('connection', (socket) => {
   console.log('👌  CONNECTED  👌')
